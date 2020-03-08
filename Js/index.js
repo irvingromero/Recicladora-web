5011b8e399d1e317ebadd9048e26aa287aa7e6b4
Irving <i.romerog93@gmail.com>
2020-03-07 21:11:01 -0800

    v1.0

---
+++ Js/index.js
@@ -0,0 +1,7 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), 
+    {
+        center: {lat: 32.624862, lng: -115.448325},
+        zoom: 13.5,
+    });
+}
--- Js/logica.js
+++
@@ -1,7 +0,0 @@
-function iniciarMapa(){
-    var loc = {lat: 32.6248622, lon: -115.448325};
-    var map = new google.maps.Map(document.getElementById('mapa'), {
-        zoom: 4,
-        center: loc
-    });
-}
